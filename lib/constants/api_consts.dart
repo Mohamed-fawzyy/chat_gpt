a2d4e377c51cbbab68a64d0c1a283cf31abff1a8
fawzy <fwzymohamed90@gmail.com>
2023-04-18 03:44:56 +0200

    Last Commit

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: non_constant_identifier_names
 
 String BASE_URL = 'https://api.openai.com/v1';
-String API_KEY = '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';
+String API_KEY = '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';
+//Put your API_KEY
